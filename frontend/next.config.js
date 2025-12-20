437d1e9b9c42d9fd458608f4b34e969d033062c2
LoftyComet <1277173875@qq.com>
2025-12-21 05:12:54 +0800

    服务器配置

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // 必须保留这个，Docker 才能构建成功
+  
+  // 合并 typescript 配置
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 
   async rewrites() {
     return [
